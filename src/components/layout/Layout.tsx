a8ecba846e291779797d50be13018c0329bb4f5b
Luki1235512 <Luki1235512@gmail.com>
2023-01-30 11:28:28 +0100

    Add navbar component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React, {ReactNode} from "react";
+import Navbar from "@/src/components/navbar/Navbar";
+
+type LayoutProps = {
+    children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
+    return (
+        <>
+            <Navbar />
+            <main>{children}</main>
+        </>
+    );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app'
 import {ChakraProvider} from "@chakra-ui/provider";
 import {theme} from "../chakra/theme";
+import Layout from "@/src/components/layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+          <Layout>
+              <Component {...pageProps} />
+          </Layout>
       </ChakraProvider>
-  )
+  );
 }
 
 export default MyApp;
