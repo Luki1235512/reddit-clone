0e2b8af7c889b1e40c6b3715acf6ede429d667a2
Luki1235512 <Luki1235512@gmail.com>
2023-01-30 10:57:00 +0100

    New packages and src directory

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/src/styles/globals.css'
+import type { AppProps } from 'next/app'
+import {ChakraProvider} from "@chakra-ui/provider";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
+}
+
+export default MyApp;
