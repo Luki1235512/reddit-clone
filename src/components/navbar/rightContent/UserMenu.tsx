285d68a2914ff76bb92a0f6805c311cb5e1d003c
Luki1235512 <Luki1235512@gmail.com>
2023-02-02 13:01:18 +0100

    Add user karma

--- src/components/navbar/rightContent/UserMenu.tsx
+++ src/components/navbar/rightContent/UserMenu.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Button, Flex, Icon, Menu, MenuButton, MenuDivider, MenuItem, MenuList } from "@chakra-ui/react";
+import { Flex, Icon, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text } from "@chakra-ui/react";
 import { signOut, User } from "firebase/auth";
 import React from "react";
 import { FaRedditSquare } from "react-icons/fa";
@@ -26,6 +26,21 @@ const UserMenu: React.FC<UserMenuProps> = ({user}) => {
                         {user ? (
                         <>
                             <Icon fontSize={24} mr={1} color="gray.300" as={FaRedditSquare} />
+                            <Flex
+                                direction="column"
+                                display={{base: "none", lg: "flex"}}
+                                fontSize="8pt"
+                                align="flex-start"
+                                mr={8}
+                            >
+                                <Text fontWeight={700}>
+                                    {user?.displayName || user.email?.split("@")[0]}
+                                </Text>
+                                <Flex>
+                                    <Icon as={IoSparkles} color="brand.100" mr={1} />
+                                    <Text color="gray.400">1 karma</Text>
+                                </Flex>
+                            </Flex>
                         </>
                         ) : (
                             <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
