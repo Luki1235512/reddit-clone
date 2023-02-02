bde48e785b13ed73829fd8ee3026f23a351c8e7c
Luki1235512 <Luki1235512@gmail.com>
2023-02-02 13:48:14 +0100

    Add create community modal skeleton

--- src/components/navbar/directory/Directory.tsx
+++ src/components/navbar/directory/Directory.tsx
@@ -2,6 +2,7 @@ import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Flex, Icon, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text } from "@chakra-ui/react";
 import React from "react";
 import { TiHome } from "react-icons/ti";
+import Communities from "./Communities";
 
 const Directory: React.FC = () => {
 
@@ -28,8 +29,7 @@ const Directory: React.FC = () => {
                 </Flex>
             </MenuButton>
             <MenuList>
-                {/* <Communities /> */}
-                Communities
+                <Communities />
             </MenuList>
         </Menu>
     );
