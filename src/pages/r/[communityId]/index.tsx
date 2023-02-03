c4a0644ee73a279ae2a4f10e70b87ab1ca74d627
Luki1235512 <Luki1235512@gmail.com>
2023-02-03 13:21:24 +0100

    Add community page

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,34 @@
+import { Community } from "@/src/atoms/communitiesAtom";
+import { firestore } from "@/src/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import safeJsonStringify from "safe-json-stringify";
+
+
+type CommunityPageProps = {
+    communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({communityData}) => {
+    return <div>WELCOME TO {communityData.id}</div>
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    // GET COMMUNITY DATA AND PASS IT TO CLIENT
+    try {
+        const communityDocRef = doc(firestore, "communities", context.query.communityId as string);
+        const communityDoc = await getDoc(communityDocRef);
+
+        return {
+            props: {
+                communityData: JSON.parse(safeJsonStringify({id: communityDoc.id, ...communityDoc.data()})),
+            }
+        };
+    }
+    catch (error) {
+        // TODO: ADD ERROR PAGE HERE
+        console.log("getServerSideProps error", error);
+    }
+};
+
+export default CommunityPage;
