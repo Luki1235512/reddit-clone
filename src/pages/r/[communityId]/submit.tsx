0c0224da6ad8dcbb3b4c1c13b4bd4857b22de2a6
Luki1235512 <Luki1235512@gmail.com>
2023-02-08 12:34:54 +0100

    Add NewPostForm component

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,5 +1,23 @@
+import PageContent from "@/src/components/layout/PageContent";
+import NewPostForm from "@/src/components/posts/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
+
 const SubmitPostPage: React.FC = () => {
-    return <div>Submit page</div>
+    return (
+        <PageContent>
+            <>
+                <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+                    <Text>Create a post</Text>
+                </Box>
+                <NewPostForm />
+                {/* NewPostForm */}
+            </>
+
+            <>
+                {/* About */}
+            </>
+        </PageContent>
+    )
 };
 
 export default SubmitPostPage;
